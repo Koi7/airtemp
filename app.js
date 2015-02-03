143210c2ec2025764eee89924fb88dea66deb5e7
Ramazan Izmaylov <shunen666@gmail.com>
2015-02-03 19:05:04 +0300

    Autocomplete realized
    using Google Maps API

---
+++ app.js
@@ -0,0 +1,16 @@
+/*var init = function  () {
+	var input = document.getElementById("city");
+	var options = {
+		types: ['(regions)']
+	};
+
+	var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+	    var place = autocomplete.getPlace(); //получаем место
+	    console.log(place);
+	    console.log(place.name);  //название места
+	    console.log(place.id);  //уникальный идентификатор места
+    });
+}
+google.maps.event.addDomListener(window, 'load', init);*/
